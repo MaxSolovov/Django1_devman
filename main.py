8c8a34e6d6fc5ba11d40d1eabe6ae282efc34ff3
Max <maxvmd@mail.ru>
2022-07-01 23:41:11 +0300

    Django1

--- main.py
+++ main.py
@@ -9,4 +9,19 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Количество пропусков:', Passcard.objects.count(), "\n")  # noqa: T001
+    passcards = Passcard.objects.all()
+    #print(*passcards, sep="\n")
+    some_person = passcards[10]
+    print(f"owener_name: {some_person.owner_name}")
+    print(f"passcode: {some_person.passcode}")
+    print(f"created_at: {some_person.created_at}")
+    print(f"is_active: {some_person.is_active}")
+    print()
+    print(f"Всего пропусков: {len(passcards)}")
+    print(f"Активных пропусков: {len(list(filter(lambda x: x.is_active, passcards)))}")
+    print()
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print(f"Всего пропусков: {len(passcards)}")
+    print(f"Активных пропусков: {len(active_passcards)}")
+
